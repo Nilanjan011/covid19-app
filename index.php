bfd8d34e7cc0c252078dad4d2682020d1f50edca
Nilanjan011 <67472097+Nilanjan011@users.noreply.github.com>
2020-08-10 18:54:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -89,7 +89,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script>
-        fetch('http://localhost/covidx/data.json')
+        //fetch('http://localhost/covidx/data.json')
+        fetch('https://api.covid19api.com/summary')
          .then((apidata)=>{
         //    console.log(apidata);
             return apidata.json();
